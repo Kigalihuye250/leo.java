53b96a045a13dee70d520de09c0dbf9ea99847ae
Kigalihuye250 <151157727+Kigalihuye250@users.noreply.github.com>
2023-11-17 13:58:18 +0200

    Add files via upload

---
+++ java/output of the body of ifelse.java
@@ -0,0 +1,14 @@
+class Test {
+
+    public static void main(String[] args) {
+        int num1 = 20, num2 = 10;
+
+        if (num1 < num2) {
+            System.out.println("num2 is greater than num1");
+        } else {
+            System.out.println("num2 is less than num1");
+        }
+
+        System.out.println("This statement is outside the body of if and else");
+    }
+}
